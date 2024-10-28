5185904e1fb17d755836f1341cf3f106936e62ef
yanet-y <107769377+yanet-y@users.noreply.github.com>
2024-10-28 21:26:08 +0300

    Add files via upload

---
+++ exercise5.js
@@ -0,0 +1,11 @@
+//Exercise 1
+const add = (a, b) => a + b;
+//Exercise 2
+const multiply = (a, b) => a * b;
+//Exercise 3
+const greet = (name) => `Hello, ${name}`;
+
+
+console.log(add(3,4))
+console.log(multiply(4,5))
+console.log(greet("Yanet"))
